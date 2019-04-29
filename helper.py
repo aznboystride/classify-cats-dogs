9f9e16c6cbea6d4e7a957eeee573c3c7023583f1
root <root@instance-1.us-east1-b.c.diesel-rhythm-238415.internal>
2019-04-29 07:14:14 +0000

    its time to train model

--- helper.py
+++ helper.py
@@ -6,27 +6,29 @@ from sklearn.model_selection import train_test_split
 from keras.utils import to_categorical
 
 def create_train_data(TRAIN_DIR, SIZE=150):
-    dirs = os.listdir('data-set')
+    dirs = os.listdir('.data-set')
     if "x_train.npy" in dirs and "y_train.npy" in dirs and \
         "x_test.npy" in dirs and "y_test.npy" in dirs: 
-        return np.load("data-set/x_train.npy"), np.load("data-set/y_train.npy"), np.load("data-set/x_test.npy"), np.load("data-set/y_test.npy")
+        return np.load(".data-set/x_train.npy"), np.load(".data-set/y_train.npy"), np.load(".data-set/x_test.npy"), np.load(".data-set/y_test.npy")
  
     images = []
     labels = []
-    images1= []
-    labels1= []
+    
     for file in tqdm(os.listdir(TRAIN_DIR)):
         label = 0 if file.split('.')[0] == 'cat' else 1
         image = cv2.resize(cv2.imread(os.path.join(TRAIN_DIR,file), cv2.IMREAD_GRAYSCALE), (SIZE, SIZE))
         images.append(image)
         labels.append(label)
-  
-    data = train_test_split((np.array(images)/255).reshape(-1, SIZE, SIZE, 1), to_categorical(labels), test_size=0.2)
-     
-    if "x_train.npy" not in dirs: np.save("data-set/x_train.npy", data[0])
-    if "y_train.npy" not in dirs: np.save("data-set/y_train.npy", data[1])
-    if "x_test.npy" not in dirs: np.save("data-set/x_test.npy", data[2])
-    if "y_test.npy" not in dirs: np.save("data-set/y_test.npy", data[3])
+    images = (np.array(images)/255).reshape(-1, SIZE, SIZE, 1)
+    labels = to_categorical(labels)
+    data = train_test_split(images, labels, test_size=0.2)
+    
+    print("Saving data where needed")
+ 
+    #if "x_train.npy" not in dirs: np.save(".data-set/x_train.npy", data[0])
+    #if "y_train.npy" not in dirs: np.save(".data-set/y_train.npy", data[1])
+    #if "x_test.npy" not in dirs: np.save(".data-set/x_test.npy", data[2])
+    #if "y_test.npy" not in dirs: np.save(".data-set/y_test.npy", data[3])
 
     return data
 
--- learn.py
+++ learn.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 import keras
 from keras.models import Sequential,Input,Model
 from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D, Activation
 from keras.layers.normalization import BatchNormalization
 from keras.layers.advanced_activations import LeakyReLU
 from PIL import Image
@@ -17,15 +17,46 @@ batch_size = 64
 epochs = 10
 num_classes = 2
 
-TRAIN_DIR = "/Users/Pey/Downloads/train"
-TEST_DIR  = "/Users/Pey/Downloads/test"
-SIZE = 150
+TRAIN_DIR = "../train"
+SIZE = 227
 
 
 print("Loading Dataset...")
 
-x_train, y_train, x_test, y_test = create_train_data(TRAIN_DIR)
+x_train, y_train, x_test, y_test = create_train_data(TRAIN_DIR, SIZE)
 
 print("x_train shape: {}\ny_train shape: {}\nx_test.shape: {}\ny_test.shape: {}".format(x_train.shape, y_train.shape, x_test.shape, y_test.shape))
 
- 
+print("\nCreating convolutional neural network...")
+
+model = Sequential()
+model.add(Conv2D(filters=96, kernel_size=(11,11), input_shape=(SIZE, SIZE, 1), padding="valid", strides=(4,4)))
+model.add(LeakyReLU(alpha=.001))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+model.add(Conv2D(filters=256, kernel_size=(11,11), strides=(1,1), padding='valid'))
+model.add(LeakyReLU(alpha=.001))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+model.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='valid'))
+model.add(LeakyReLU(alpha=.001))
+model.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='valid'))
+model.add(LeakyReLU(alpha=.001))
+model.add(Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='valid'))
+model.add(LeakyReLU(alpha=.001))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+model.add(Flatten())
+model.add(Dense(4096, input_shape=(224*224*3,)))
+model.add(LeakyReLU(alpha=.001))
+model.add(Dropout(0.4))
+model.add(Dense(4096))
+model.add(LeakyReLU(alpha=.001))
+model.add(Dropout(0.4))
+
+model.add(Dense(1000))
+model.add(LeakyReLU(alpha=.001))
+model.add(Dropout(0.4))
+model.add(Dense(17))
+model.add(Activation('softmax'))
+
+print("Model summary: {}".format(model.summary()))
+
+
